78604c524adfecd8c9015799fe2816bab7b1417d
preethamdheeraj <chpreetham45@gmail.com>
2023-07-19 22:33:47 +0530

    ADD more course files completed challenges

--- The2023WebDeveloperBootcamp-coltSteele/allcodingFilesPractice.js
+++ The2023WebDeveloperBootcamp-coltSteele/allcodingFilesPractice.js
@@ -1,7 +1,4 @@
-// now we will be learning about the modifying of objects
-const midterms = { danielle: 96, thomas: 78 };
-console.log(midterms);
-// now we should be changing thomas score
-midterms.thomas = 79;
-console.log(midterms);
-// now the container value will be changed and will reflect the new value
+const animals = ['lions', 'tigers', 'bears'];
+for (let i = 0; i <= animals.length; i++) {
+    console.log([i], animals[i]);
+}
