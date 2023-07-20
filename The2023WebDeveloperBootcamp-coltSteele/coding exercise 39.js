4ce26fb92fb5cb9ef7a2bcc7c0b1f56fb0a95b2d
preethamdheeraj <chpreetham45@gmail.com>
2023-07-20 15:59:29 +0530

    ADD files of the course completed

---
+++ The2023WebDeveloperBootcamp-coltSteele/coding exercise 39.js
@@ -0,0 +1,12 @@
+// DEFINE YOUR FUNCTION BELOW:
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+isShortsWeather(80);
+isShortsWeather(48);
+isShortsWeather(75);
