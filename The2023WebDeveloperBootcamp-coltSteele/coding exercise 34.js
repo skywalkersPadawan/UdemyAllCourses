a71a353837c7393b4511093b5a61f1b77de64526
preethamdheeraj <chpreetham45@gmail.com>
2023-07-20 13:57:30 +0530

    ADD more completed course files

---
+++ The2023WebDeveloperBootcamp-coltSteele/coding exercise 34.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let loopNumbers of numbers) {
+    console.log(loopNumbers * loopNumbers);
+}
