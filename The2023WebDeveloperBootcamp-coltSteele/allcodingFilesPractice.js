4d024699dbafd59f7e7a85bee09c118ab4bcb7cc
preethamdheeraj <chpreetham45@gmail.com>
2023-07-25 15:07:09 +0530

    ADD course completed sections and projects

--- The2023WebDeveloperBootcamp-coltSteele/allcodingFilesPractice.js
+++ The2023WebDeveloperBootcamp-coltSteele/allcodingFilesPractice.js
@@ -1,1 +1,16 @@
-// now let us talk about removing elements and removing child
+function twist() {
+    console.log('this is twisting operation!');
+}
+
+function shout() {
+    console.log('this is shout operation!');
+}
+
+const twistAndShoutButton = document.querySelector('#twistAndShout');
+// twistAndShoutButton.onclick = shout;
+// twistAndShoutButton.onclick = twist;
+/* we cannot have two different call back functions for the same event using the same onclick */
+/* so we will be using addEventListener */
+
+twistAndShoutButton = addEventListener('click', twist, { once: true });
+twistAndShoutButton = addEventListener('click', shout, { once: true });
